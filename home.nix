55ac15b2096e993d8a434c9e9b6a72e999228e9b
Duc Tran <duc.tran2027@gmail.com>
2025-06-11 13:37:21 -0400

    fix lockscreen

--- home.nix
+++ home.nix
@@ -227,6 +227,7 @@
       "$fileManager" = "dolphin";
       "$menu" = "wofi --show drun";
       "$browser" = "librewolf";
+      "$lockCmd" = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
 
       # Autostart
       exec-once = [
@@ -360,9 +361,9 @@
         # Window management
         "$mainMod, Q, killactive,"
         "$mainMod SHIFT, Q, exit,"
-        "$mainMod, -, exec, $fileManager"
         "$mainMod, P, pseudo,"
         "$mainMod, J, togglesplit,"
+        "$mainMod SHIFT, RETURN, exec, $lockCmd"
 
         # Application launchers
         "$mainMod, RETURN, exec, $terminal"
@@ -370,7 +371,6 @@
         "$mainMod, D, exec, $menu"
         "$mainMod, E, exec, $fileManager"
         "$mainMod, V, exec, $terminal --class=clipse -e clipse"
-        "$mainMod SHIFT, L, exec, hyprlock"
 
         # Move focus (vim-like)
         "$mainMod, H, movefocus, l"
@@ -642,40 +642,90 @@
     enable = true;
     settings = {
       general = {
-        grace = 300;
-        hide_cursor = true;
+        hide_cursor = false;
       };
 
       auth = {
         fingerprint = {
           enabled = true;
-          ready_message = "Scan fingerprint to unlock";
-          present_message = "Scanning...";
-          retry_delay = 250; # in milliseconds
         };
+      };
+
+      animations = {
+        enabled = true;
+        bezier = "linear, 1, 1, 0, 0";
+        animation = [
+          "fadeIn, 1, 5, linear"
+          "fadeOut, 1, 5, linear"
+          "inputFieldDots, 1, 2, linear"
+        ];
       };
 
       background = [
         {
+          monitor = "";
           path = "screenshot";
           blur_passes = 3;
-          blur_size = 8;
         }
       ];
 
       input-field = [
         {
-          size = "200, 50";
-          position = "0, -80";
           monitor = "";
-          dots_center = true;
+          size = "20%, 5%";
+          outline_thickness = 3;
+          inner_color = "rgba(0, 0, 0, 0.0)"; # no fill
+          outer_color = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          check_color = "rgba(00ff99ee) rgba(ff6633ee) 120deg";
+          fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
+          font_color = "rgb(143, 143, 143)";
           fade_on_empty = false;
-          font_color = "rgb(202, 211, 245)";
-          inner_color = "rgb(91, 96, 120)";
-          outer_color = "rgb(24, 25, 38)";
-          outline_thickness = 5;
-          placeholder_text = "Password...";
-          shadow_passes = 2;
+          rounding = 15;
+          font_family = "Monospace";
+          placeholder_text = "Input password...";
+          fail_text = "$PAMFAIL";
+          # uncomment to use a letter instead of a dot to indicate the typed password
+          # dots_text_format = "*";
+          # dots_size = 0.4;
+          dots_spacing = 0.3;
+          # uncomment to use an input indicator that does not show the password length (similar to swaylock's input indicator)
+          # hide_input = true;
+          position = "0, -20";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      label = [
+        # TIME
+        {
+          monitor = "";
+          text = "$TIME"; # ref. https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/#variable-substitution
+          font_size = 90;
+          font_family = "Monospace";
+          position = "-30, 0";
+          halign = "right";
+          valign = "top";
+        }
+        # DATE
+        {
+          monitor = "";
+          text = "cmd[update:60000] date +\"%A, %d %B %Y\""; # update every 60 seconds
+          font_size = 25;
+          font_family = "Monospace";
+          position = "-30, -150";
+          halign = "right";
+          valign = "top";
+        }
+        # LAYOUT
+        {
+          monitor = "";
+          text = "$LAYOUT[en,ru]";
+          font_size = 24;
+          onclick = "hyprctl switchxkblayout all next";
+          position = "250, -20";
+          halign = "center";
+          valign = "center";
         }
       ];
     };
