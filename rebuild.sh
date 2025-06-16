3820c3bb8d16570531a0e8d74523ec656cc2469f
Duc Tran <duc.tran2027@gmail.com>
2025-06-16 12:13:59 -0400

    add rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+if git diff --quiet '*.nix'; then
+    echo "No changes detected, exiting."
+    exit 0
+fi
+nixfmt ./**/*.nix &>/dev/null \
+  || ( nixfmt . ; echo "formatting failed!" && exit 1)
+git diff -U0 ./*.nix
+echo "Home-Manager Rebuilding..."
+# shellcheck disable=SC2024
+home-manager switch &>nixos-switch.log || (
+grep --color error < nixos-switch.log  && exit 1)
+current=$(home-manager generations | head -n 1 | awk '{ print $1,$2,$3,$4,$5 }')
+git commit -am "$current"
+notify-send -e "Home-Manger Rebuilt OK!" --icon=software-update-available
