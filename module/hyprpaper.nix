b09305f166fe4d64833cac379d8347d35cc5a7c6
Duc Tran <duc.tran2027@gmail.com>
2025-06-18 22:35:10 -0400

    2025-06-18 22:35 : id 89

--- module/hyprpaper.nix
+++ module/hyprpaper.nix
@@ -3,13 +3,5 @@
   # Hyprpaper configuration (wallpaper)
   services.hyprpaper = {
     enable = true;
-    settings = {
-      preload = [
-        "~/Sync/Wallpapers_clean/"
-      ];
-      wallpaper = [
-        ",~/Sync/Wallpapers_clean/"
-      ];
-    };
   };
 }
