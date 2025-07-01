8d5b3a1f88f616e8bdf5d043553def1d6198c93a
Duc Tran <duc.tran2027@gmail.com>
2025-07-01 13:16:47 -0400

    2025-07-01 13:16 : id 151

--- user/englishlayup/home.nix
+++ user/englishlayup/home.nix
@@ -202,7 +202,7 @@
           #!/usr/bin/env bash
           # Find the PIP window address
           PIP_WINDOW=$(hyprctl clients -j | jq -r '.[] | select(.title=="Picture in picture") | .address')
-          hyprctl dispatch setprop address:$PIP_WINDOW opaque toggle'';
+          hyprctl dispatch setprop address:$PIP_WINDOW opaque toggle > /dev/null'';
         executable = true;
       };
     };
