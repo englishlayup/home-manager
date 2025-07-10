0349f64fb1b72f3a006c9214fb8475202a87eb27
Duc Tran <duc.tran2027@gmail.com>
2025-07-09 23:06:58 -0400

    2025-07-09 23:06 : id 160

--- flake.nix
+++ flake.nix
@@ -32,11 +32,6 @@
             {
               home.username = username;
               home.homeDirectory = homeDirectory;
-              wayland.windowManager.hyprland = {
-                enable = true;
-                package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-                portalPackage = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-              };
             }
             ./home.nix
           ];
