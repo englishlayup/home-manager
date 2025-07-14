5287aa95c0a4dc2c60270d3cf7013c38d6ff9ca0
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2025-07-14 16:47:19 -0400

    2025-07-14 16:47 : id 4

--- module/fish.nix
+++ module/fish.nix
@@ -4,6 +4,7 @@
     enable = true;
     interactiveShellInit = ''
       fish_hybrid_key_bindings
+      bind \ce edit_command_buffer
       set -g theme_color_scheme gruvbox-dark
       fnm env --use-on-cd --version-file-strategy=recursive --shell fish | source
     '';
