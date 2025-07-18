58b60ce6b54418adfd6801940cdee038d328d152
Duc Tran <duc.tran2027@gmail.com>
2025-07-17 23:05:18 -0400

    2025-07-17 23:05 : id 161

--- module/hyprland.nix
+++ module/hyprland.nix
@@ -130,7 +130,6 @@ in
       input = {
         kb_layout = "us,us";
         kb_variant = ",intl";
-        kb_options = "caps:escape,grp:alt_shift_toggle";
         follow_mouse = 2;
         sensitivity = 0;
 
