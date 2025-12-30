458ddd7782cf977fc33f494dfc810f16a2487a62
Duc Tran <duc.tran2027@gmail.com>
2025-12-30 11:33:09 -0500

    2025-12-30 11:33 : id 242

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -4,6 +4,7 @@ return {
     require 'lint'.linters_by_ft = {
       bzl = { 'buildifier' },
       bash = { 'shellcheck' },
+      cmake = { 'cmake_lint' },
     }
     vim.api.nvim_create_autocmd({ 'VimEnter', 'BufWritePost' }, {
       callback = function()
