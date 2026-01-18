c88d1fca48ca9723632c8eb354c0555e58f35538
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2026-01-18 13:47:30 -0500

    2026-01-18 13:47 : id 32

---
+++ module/packages.nix
@@ -0,0 +1,93 @@
+# Package lists - just data, no logic
+{ pkgs }:
+with pkgs;
+{
+  cli = [
+    tmux
+    yazi
+    hyperfine
+    delta
+    eza
+    dust
+    dua
+    git
+    btop
+    zoxide
+    tldr
+    wget
+    curl
+    unzip
+    zip
+    htop
+    neofetch
+    fd
+    ripgrep
+    fzf
+    jq
+    xxd
+    translate-shell
+    imagemagick
+    ffmpeg
+  ];
+
+  dev = [
+    # Language servers
+    lua-language-server
+    clang-tools
+    gopls
+    pyright
+    ruff
+    bash-language-server
+    buildifier
+    shellcheck
+    delve
+    nixfmt-rfc-style
+    starpls
+    nil
+    htmx-lsp
+    vscode-langservers-extracted
+    templ
+    tailwindcss-language-server
+    tree-sitter
+    cmake-language-server
+    # Compilers/runtimes
+    clang
+    uv
+    fnm
+    go
+    # Build tools
+    cmake
+    ninja
+    gnumake
+  ];
+
+  desktop = [
+    wl-clipboard
+    # Theme
+    nwg-look
+    gruvbox-gtk-theme
+    gruvbox-dark-icons-gtk
+    libsForQt5.qt5ct
+    qt6Packages.qt6ct
+    bibata-cursors
+    # Screenshot
+    slurp
+    grim
+    hyprshot
+    hyprpicker
+  ];
+
+  personal = [
+    anki-bin
+    musescore
+    mpv-unwrapped
+  ];
+
+  productivity = [
+    obsidian
+    libreoffice-qt6-fresh
+    zathura
+    xournalpp
+    gimp3
+  ];
+}
