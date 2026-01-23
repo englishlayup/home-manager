1c2fa2fcf01a548ac19898e1c355c46c3b056bfb
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2026-01-23 11:50:48 -0500

    2026-01-23 11:50 : id 61

--- fish/conf.d/init.fish
+++ fish/conf.d/init.fish
@@ -1,8 +1,3 @@
 # Fish shell initialization
 fish_hybrid_key_bindings
 set -g theme_color_scheme gruvbox-dark
-
-# Tool integrations
-fnm env --use-on-cd --version-file-strategy=recursive --shell fish | source
-uv generate-shell-completion fish | source
-uvx --generate-shell-completion fish | source
