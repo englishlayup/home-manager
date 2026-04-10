9c74c62a6c0a9541cfc59bf813b1902c8f1a1368
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2026-04-10 08:26:35 -0400

    2026-04-10 08:26 : id 3

--- windows/scripts/stage-nvim-config.ps1
+++ windows/scripts/stage-nvim-config.ps1
@@ -1,4 +1,4 @@
-$source = Join-Path $PSScriptRoot "..\nvim"
+$source = Join-Path $PSScriptRoot "..\..\nvim"
 $dest = Join-Path $env:LOCALAPPDATA "nvim"
 
 robocopy $source $dest /MIR /NFL /NDL /NJH /NJS
