f67bbac61e3a9ca4683113f3a4c6f64410625ca3
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2026-04-12 11:51:20 -0400

    2026-04-12 11:51 : id 11

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -18,7 +18,7 @@ return {
     lazygit = {}
   },
   keys = {
-    { '<leader>sf', function() Snacks.picker.smart() end,                                 desc = 'Smart [S]earch [F]iles' },
+    { '<leader>sf', function() Snacks.picker.smart() end,                   desc = 'Smart [S]earch [F]iles' },
     {
       '<leader>b',
       function()
@@ -39,16 +39,16 @@ return {
       end,
       desc = '[S]earch [T]erminal Buffers'
     },
-    { '<leader>:',  function() Snacks.picker.command_history() end,                       desc = 'Search Command History' },
-    { '<leader>sh', function() Snacks.picker.help() end,                                  desc = 'Search [H]elp Pages' },
-    { '<leader>sr', function() Snacks.picker.git_files() end,                             desc = '[S]earch Git [Repo]' },
-    { '<leader>sg', function() Snacks.picker.grep() end,                                  desc = '[S]earch [G]rep' },
-    { '<leader>sw', function() Snacks.picker.grep_word() end,                             desc = 'Search word or visual selection', mode = { 'n', 'x' } },
-    { '<leader>sk', function() Snacks.picker.keymaps() end,                               desc = '[S]earch [K]eymaps' },
-    { '<leader>sa', function() Snacks.picker.autocmds() end,                              desc = '[S]earch [A]utocmds' },
-    { '<leader>sn', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, desc = '[S]earch [N]eovim Files' },
-    { '<leader>n',  function() Snacks.picker.grep { cwd = '~/notes' } end,                desc = 'Grep ~/[n]otes' },
-    { 'grws',       function() Snacks.picker.lsp_workspace_symbols() end,                 desc = '[W]orkspace [S]ymbols' },
-    { '<leader>gg', function() Snacks.lazygit() end,                                      desc = 'Lazy[G]it' },
+    { '<leader>:',  function() Snacks.picker.command_history() end,         desc = 'Search Command History' },
+    { '<leader>sh', function() Snacks.picker.help() end,                    desc = 'Search [H]elp Pages' },
+    { '<leader>sr', function() Snacks.picker.git_files() end,               desc = '[S]earch Git [Repo]' },
+    { '<leader>sg', function() Snacks.picker.grep() end,                    desc = '[S]earch [G]rep' },
+    { '<leader>sw', function() Snacks.picker.grep_word() end,               desc = 'Search word or visual selection', mode = { 'n', 'x' } },
+    { '<leader>sk', function() Snacks.picker.keymaps() end,                 desc = '[S]earch [K]eymaps' },
+    { '<leader>sa', function() Snacks.picker.autocmds() end,                desc = '[S]earch [A]utocmds' },
+    { '<leader>sn', function() Snacks.picker.files { cwd = '~/notes' } end, desc = '[S]earch [N]otes' },
+    { '<leader>n',  function() Snacks.picker.grep { cwd = '~/notes' } end,  desc = '[G]rep [N]otes' },
+    { 'grws',       function() Snacks.picker.lsp_workspace_symbols() end,   desc = '[W]orkspace [S]ymbols' },
+    { '<leader>gg', function() Snacks.lazygit() end,                        desc = 'LazyGit' },
   },
 }
